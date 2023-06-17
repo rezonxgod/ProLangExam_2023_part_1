0722b36779e5ba89434668e25001388b9000b310
Yeslan Nurzhigitov <125284109+rezonxgod@users.noreply.github.com>
2023-06-17 06:27:49 +0200

    StringUtilities.cs

--- ProgrammingLanguagesExam/Utilities/StringUtilities.cs
+++ ProgrammingLanguagesExam/Utilities/StringUtilities.cs
@@ -1,6 +1,20 @@
-﻿namespace Utilities;
+namespace Utilities;
 
 public class StringUtilities
 {
+public static int ToWords(string input)
+    {
+        string[] words = input.Split(' ');
+        HashSet<string> uniqueWords = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
 
+        foreach (string word in words)
+        {
+            if (!string.IsNullOrWhiteSpace(word))
+            {
+                uniqueWords.Add(word);
+            }
+        }
+
+        return uniqueWords.Count;
+    }
 }

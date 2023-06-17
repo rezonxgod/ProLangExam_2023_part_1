446d64326498278d694b4ab5f0f10bb0d774dea0
Yeslan Nurzhigitov <125284109+rezonxgod@users.noreply.github.com>
2023-06-17 06:29:06 +0200

    TeacherController.cs

--- ProgrammingLanguagesExam/University/Controllers/TeacherController.cs
+++ ProgrammingLanguagesExam/University/Controllers/TeacherController.cs
@@ -1,15 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using University.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
-namespace University.Controllers;
-
-[Route("{controller}")]
-public class TestController
+namespace University.Controllers
 {
-    private readonly UniversityDbContext _universityDbContext;
+    [Route("Teacher")]
+    [ApiController]
+    public class TeacherController : ControllerBase
+    {
+        private readonly UniversityDbContext _dbContext;
 
-    public TestController(UniversityDbContext universityDbContext) => _universityDbContext = universityDbContext;
+        public TeacherController(UniversityDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
 
-    [HttpGet]
-    public string Test(string message) => $"Test: database contains {_universityDbContext.Teachers.Count()} teachers with {_universityDbContext.Skills.Count()} skills in total";
+        // GET /Teacher/All
+        [HttpGet("All")]
+        public IActionResult GetAllTeachers()
+        {
+            var teachers = _dbContext.Teachers.Select(t => new
+            {
+                t.Id,
+                t.Name
+            }).ToList();
+
+            return Ok(teachers);
+        }
+
+        // GET /Teacher/{id}
+        [HttpGet("{id}")]
+        public IActionResult GetTeacherById(int id)
+        {
+            var teacher = _dbContext.Teachers
+                .Include(t => t.Skills)
+                .FirstOrDefault(t => t.Id == id);
+
+            if (teacher == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(teacher);
+        }
+    }
 }
